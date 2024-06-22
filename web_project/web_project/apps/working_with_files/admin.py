ec6058b87e2171383b87c17802850e6a2c9d9841
Yurii Koshliak <satorxp10@gmail.com>
2024-06-22 13:49:10 +0200

    Робота з файлами (перші кроки) + gitignore

--- web_project/web_project/apps/working_with_files/admin.py
+++ web_project/web_project/apps/working_with_files/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import File
 # Register your models here.
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'get_category', 'uploaded_at']
+
+    def get_category(self, obj):
+        return obj.get_category()
+    get_category.short_description = 'Категорія'
