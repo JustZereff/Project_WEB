403692ccda4d20f47dc1da19ca44f2fc328e8f25
Artem <154351406+JustZereff@users.noreply.github.com>
2024-06-29 14:44:40 +0300

    try news

--- web_project/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-
-# Это гарантирует, что app будет загружен при запуске Django
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
