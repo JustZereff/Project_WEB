fd875e8c563591807249d1fefa682afd9881fabc
Artem <154351406+JustZereff@users.noreply.github.com>
2024-07-07 09:20:31 +0300

    Update views.py

--- web_project/apps/working_with_files/views.py
+++ web_project/apps/working_with_files/views.py
@@ -64,7 +64,10 @@ def file_delete(request, pk):
     
     if request.method == 'POST':
         try:
-            destroy(file.public_id)
+            file_url = file.file.url
+            public_id = '/'.join(file.file.name.split('/')[-2:])
+            public_id = public_id.replace('.' + file_url.split('.')[-1], '')
+            destroy(public_id)
             file.delete()
         except Exception as e:
             print(f"Error deleting file {file.name}: {str(e)}")
